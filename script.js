4ad8e75a56ba3e921d132216a40952a847aff1a0
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-12-04 20:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const delay = document.getElementById("delay");
+const text = document.getElementById("text");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+let promise = () => {
+	return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+		resolve(text.value);
+	},delay.value);
+  })
+};
+
+async function asyncFun() {
+	const data = await promise();
+	output.innerHTML = `<h1>${data}</h1>`;
+}
+
+btn.addEventListener("click",()=>{
+	asyncFun();
+})
+
+
+
